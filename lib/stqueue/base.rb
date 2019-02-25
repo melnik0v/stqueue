9e3b8b63eb3cce59e75650b4da4e3d4e5830e264
Alexey Melnikov <alexbeat96@gmail.com>
2019-02-25 19:07:36 +0600

    fix before_enqueue behaviour in test env

--- lib/stqueue/base.rb
+++ lib/stqueue/base.rb
@@ -6,6 +6,8 @@ module STQueue
 
     included do
       after_perform do
+        return unless STQueue.enabled
+
         lock_info = Utils.lock_info(queue_name)
         until lock_info
           sleep 1
@@ -21,6 +23,8 @@ module STQueue
       end
 
       before_enqueue do
+        return unless STQueue.enabled
+
         STQueue::Process.find_by(queue_name: queue_name).increase_busy
       end
     end
